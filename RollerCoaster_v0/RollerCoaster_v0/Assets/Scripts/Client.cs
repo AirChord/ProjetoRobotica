816de63dac569f9a9b37b51fe2b191546b6849d6
AirchordStudent <a11611@alunos.ipca.pt>
2020-02-22 13:51:09 +0000

    TCP connection with comments

--- RollerCoaster_v0/RollerCoaster_v0/Assets/Scripts/Client.cs
+++ RollerCoaster_v0/RollerCoaster_v0/Assets/Scripts/Client.cs
@@ -9,152 +9,114 @@ using System.Text;
 
 public class Client //: MonoBehaviour
 {
-    /*
-    public static Client Instance;
+
     public static int dataBufferSize = 4096;
 
-    public static string ip = "192.168.10.123";
+    public static string ip = "192.168.10.123"; //kuka = "192.168.10.254"
     public static int port = 7000;
-    public TCP tcp;
+    public TcpClient _socket;
+    private NetworkStream readStream;
+    private NetworkStream writeStream;
+    private byte[] receiveBuffer;
 
-    private void Awake()
+    /// <summary>
+    /// Start connecting to Server
+    /// </summary>
+    public void Connect()
     {
-        if (Instance == null)
+        Debug.Log("TryConnect");
+        this._socket = new TcpClient();
+        receiveBuffer = new byte[dataBufferSize];
+        this._socket.BeginConnect(System.Net.IPAddress.Parse(ip), port, ConnectCallback, this._socket); //Start async connection
+
+    }
+
+    /// <summary>
+    /// End connecting to server process and start reading process
+    /// </summary>
+    /// <param name="ia"></param>
+    private void ConnectCallback(IAsyncResult ia)
+    {
+        this._socket.EndConnect(ia);    //
+        if (!this._socket.Connected)
         {
-            Instance = this;
+            Debug.Log("Unnable to Connect!");
+            return;
         }
-        else
+        this._socket.NoDelay = true;    //parameter to accelerate transmission
+        readStream = this._socket.GetStream();
+        readStream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+    }
+
+    /// <summary>
+    /// Activated when buffer receive something
+    /// </summary>
+    /// <param name="ia"></param>
+    private void ReceiveCallback(IAsyncResult ia)
+    {
+        try
         {
-            Debug.Log("Instance already exists, destroying object!");
-            Destroy(this);
-        }
-    }
-
-    private void Start()
-    {
-        //tcp = new TCP();
-    }
-    public void ConnectToServer()
-    {
-        Debug.Log("ConectServer");
-        this.tcp = new TCP();
-        this.tcp.Connect();
-    }
-
-    public void DiconnectServer()
-    {
-        Debug.Log("DiconectServer");
-        this.tcp.Disconnect();
-    }
-
-    public void SendToServer(string data)
-    {
-        Debug.Log("Send2Server");
-        this.tcp.Send(data);
-    }
-    
-    public class TCP
-    {
-    */
-    //public TcpClient socket;
-        /// <summary>
-        /// ////////////////////////////////////////////////
-        /// </summary>
-        public static int dataBufferSize = 4096;
-
-        public static string ip = "192.168.10.123";
-        public static int port = 7000;
-       /// <summary>
-       /// ////////////////////////////////////////////////
-       /// </summary>
-        public TcpClient _socket;
-        private NetworkStream readStream;
-        private NetworkStream writeStream;
-        private byte[] receiveBuffer;
-
-        public void Connect(TcpClient cl)
-        {
-            Debug.Log("TryConnect");
-            this._socket = cl;
-            //{
-            //    ReceiveBufferSize = dataBufferSize,
-            //    SendBufferSize = dataBufferSize
-            //};
-
-            receiveBuffer = new byte[dataBufferSize];
-            this._socket.BeginConnect(System.Net.IPAddress.Parse(ip), port, ConnectCallback, this._socket);
-            
-        }
-
-        private void ConnectCallback(IAsyncResult ia)
-        {
-            this._socket.EndConnect(ia);
-            if (!this._socket.Connected)
+            int _byteLenght = readStream.EndRead(ia);
+            if (_byteLenght <= 0)
             {
-                Debug.Log("Unnable to Connect!");
-                return;
-            }
-
-            readStream = this._socket.GetStream();
-            readStream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
-            Send("Hello\n");
-        }
-
-        private void ReceiveCallback(IAsyncResult ia)
-        {
-            try
-            {
-                int _byteLenght = readStream.EndRead(ia);
-                if (_byteLenght <= 0)
-                {
-                    //TODO: diconnect
-                    Disconnect();
-                }
-
-                byte[] _data = new byte[_byteLenght];
-                Array.Copy(receiveBuffer, _data, _byteLenght);
-                string newMessage = System.Text.Encoding.UTF8.GetString(_data, 0, _byteLenght);
-                Debug.Log(newMessage);
-            }
-            catch
-            {
-                //TODO: disconnect
+                //TODO: diconnect
                 Disconnect();
             }
-        }
 
-        public void Disconnect()
+            byte[] _data = new byte[_byteLenght];
+            Array.Copy(receiveBuffer, _data, _byteLenght);
+            string newMessage = System.Text.Encoding.UTF8.GetString(_data, 0, _byteLenght);
+            Debug.Log(newMessage);
+        }
+        catch
         {
-            Debug.Log("socket close");
-            this._socket.Close();
+            //TODO: disconnect
+            Disconnect();
         }
+    }
 
-        public void Send(string message)
+    /// <summary>
+    /// Close Comunication
+    /// </summary>
+    public void Disconnect()
+    {
+        Debug.Log("socket close");
+        this._socket.Close();
+    }
+
+    /// <summary>
+    /// Start sending process
+    /// </summary>
+    /// <param name="message"></param>
+    public void Send(string message)
+    {
+        Debug.Log("startSending");
+        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(message);
+
+        if (!this._socket.Connected)
         {
-            Debug.Log("startSending");
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(message);
-
-            if (!this._socket.Connected)
-            {
-                Debug.Log("SocketnotConnected!");
-                return;
-            }
-
-            writeStream = this._socket.GetStream();
-            writeStream.BeginWrite(buffer, 0, buffer.Length, (asyncWriteResult) =>
-            {
-                //Console.Write("w");
-                writeStream.EndWrite(asyncWriteResult);
-                //src.BeginRead(buffer, 0, buffer.Length, cbk, null);
-            }, null);
-            //stream.Write(buffer, 0, buffer.Length);
+            Debug.Log("SocketnotConnected!");
+            return;
         }
 
-        private void WriteCallback(IAsyncResult ia)
-        {
+        writeStream = this._socket.GetStream();
+        //writeStream.BeginWrite(buffer, 0, buffer.Length, (asyncWriteResult) =>
+        //{
+        //        //Console.Write("w");
+        //        writeStream.EndWrite(asyncWriteResult);
+        //        //src.BeginRead(buffer, 0, buffer.Length, cbk, null);
+        //    }, null);
+        writeStream.BeginWrite(buffer, 0, buffer.Length, WriteCallback, null);
+    }
+    /// <summary>
+    /// End send process
+    /// </summary>
+    /// <param name="ia"></param>
+    private void WriteCallback(IAsyncResult ia)
+    {
 
-            writeStream.EndWrite(ia);
-        }
+        writeStream.EndWrite(ia);
+    }
     //}
 }
 
--- RollerCoaster_v0/RollerCoaster_v0/Assets/Scripts/Teste.cs
+++ RollerCoaster_v0/RollerCoaster_v0/Assets/Scripts/Teste.cs
@@ -8,40 +8,32 @@ using System.Net.Sockets;
 
 public class Teste : MonoBehaviour
 {
-    Client com;
-    TcpClient tclient;
+    Client com; //object of costum class Client, responsible for tcp communication
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start");
         com = new Client();
-        tclient = new TcpClient();
-        com.Connect(tclient);
+        //tclient = new TcpClient();
+        com.Connect();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (Input.GetKey("e"))
-        {
-            Debug.Log("update");
-            
-            //Client.Instance.ConnectToServer();
-        }
 
         if (Input.GetKey("c"))
         {
             Debug.Log("closing");
             com.Disconnect();
-            //Client.Instance.DiconnectServer();
         }
 
         if (Input.GetKey("s"))
         {
             Debug.Log("sending");
             com.Send("tyu");
-            //Client.Instance.DiconnectServer();
         }
     }
 }
