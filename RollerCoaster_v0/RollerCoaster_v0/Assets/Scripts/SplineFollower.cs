96f704eee079da71dc4456ea8ba3840f9b719577
AirchordStudent <a11611@alunos.ipca.pt>
2020-02-26 10:55:24 +0000

    Added more terrain

--- RollerCoaster_v0/RollerCoaster_v0/Assets/Scripts/SplineFollower.cs
+++ RollerCoaster_v0/RollerCoaster_v0/Assets/Scripts/SplineFollower.cs
@@ -10,7 +10,7 @@ public class SplineFollower : MonoBehaviour
     
     public GameObject splineRoot1;
     public float speed;
-
+    private bool start = false;
     //tcpSocket conn;
 
     private VR vr;
@@ -32,37 +32,14 @@ public class SplineFollower : MonoBehaviour
         if (Input.GetKey("d"))
         {
             //TCP.instance.ConnectToServer();
+            start = true;
         }
 
-
-        //vr.MoveAlongSpline(splineRoot1, gameObject, speed, VR.TRAVEL_MODE_ONCE);
-        //bool down = Input.GetKeyDown(KeyCode.Space);
-        //if (Input.GetKey ("d"))
-        //{
-        //    string ip = "192.168.10.123";
-        //    int port = 7000;
-        //    conn = new tcpSocket();
-        //    conn.Start(System.Net.IPAddress.Parse(ip), port);
-
-        //    //conn.Send("Ola");
-
-        //    Debug.Log("D pressed");
-        //    //    
-
-
-        //    // TRAVEL_MODE_LOOP, TRAVEL_MODE_ONCE, TRAVEL_MODE_TO_AND_FRO are all static constants of the class VR. Hence they can be accessed using the class name VR and no need an instance of the VR class to access these.
-        //    //vr.MoveAlongSpline(splineRoot1, gameObject, speed, VR.TRAVEL_MODE_ONCE);
-        //}
-        //if (Input.GetKey("s"))
-        //{
-        //    conn.Send("Ola");
-        //    Debug.Log("S presses");
-        //}
-        //if (Input.GetKey("e"))
-        //{
-        //    conn.Close();
-        //    Debug.Log("E presses");
-        //}
+        if (start)
+        {
+            vr.MoveAlongSpline(splineRoot1, gameObject, speed, VR.TRAVEL_MODE_ONCE);
+        }
+        
 
     }
 
@@ -71,80 +48,5 @@ public class SplineFollower : MonoBehaviour
         //conn.Close();
     }
 
-    /*
-    public class tcpSocket
-    {
-        #region Data
-        public IPAddress serverIp;
-        public int port;
-        TcpClient connection;
-        byte[] readBuffer = new byte[5000];
-        string data;
-
-        TcpListener listener;
-
-        NetworkStream stream
-        {
-            get
-            {
-                return connection.GetStream();
-            }
-        }
-
-        #endregion
-
-
-        public bool Start(IPAddress address, int _port)
-        {
-            serverIp = address;
-            port = _port;
-
-            connection = new TcpClient();
-            try
-            {
-                connection.BeginConnect(serverIp, port, (ar) => EndConnect(ar), null);
-                return (true);
-            }
-            catch (Exception e)
-            {
-                Debug.Log("Unnable to connect! Exception=" + e);
-                return (false);
-            }
-        }
-
-        private void EndConnect(IAsyncResult ar)
-        {
-            connection.EndConnect(ar);
-
-            stream.BeginRead(readBuffer, 0, readBuffer.Length, OnRead, null);
-        }
-
-        private void OnRead(IAsyncResult ar)
-        {
-            int lenght = stream.EndRead(ar);
-            if (lenght <= 0)
-            {
-                print("Connection lost!!");
-                return;
-            }
-            data = System.Text.Encoding.UTF8.GetString(readBuffer, 0, lenght);
-
-            stream.BeginRead(readBuffer, 0, readBuffer.Length, OnRead, null);
-
-        }
-
-        public void Send(string message)
-        {
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(message);
-
-            stream.Write(buffer, 0, buffer.Length);
-        }
-
-        internal void Close()
-        {
-            connection.Close();
-        }
-    }
-
-    */
+    
 }
