fa8dcdea6d3e8239e056fe2547de391155238016
AirchordStudent <a11611@alunos.ipca.pt>
2020-02-27 17:58:52 +0000

    Added Menu Scene

---
+++ RollerCoaster_v0/RollerCoaster_v0/Assets/Scripts/MenuScripts/MenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject loadingImage1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void StartRide(string LevelName)
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    public void ConfByKukaPressed()
+    {
+        this.loadingImage1.SetActive(true);
+    }
+}
---
+++ RollerCoaster_v0/RollerCoaster_v0/Assets/Scripts/SplineFollowerImage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SPEngine;
+using System;
+
+
+public class SplineFollowerImage : MonoBehaviour
+{
+    
+    public GameObject splineRoot1;
+    public float speed;
+
+    //tcpSocket conn;
+
+    private VR vr;
+    // Use this for initialization
+    void Start()
+    {
+        vr = new VR();
+        
+    }
+
+    
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+   
+            vr.MoveAlongSpline(splineRoot1, gameObject, speed, VR.TRAVEL_MODE_LOOP);
+
+        
+
+    }
+
+
+    
+}
